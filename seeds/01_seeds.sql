57fc22d4651d095670d12f4220caf742c6176fa6
Richard Miao <richardmiao1645@gmail.com>
2023-01-10 13:21:52 -0800

    Fix seed file variables

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -4,7 +4,7 @@ VALUES ('Steve Bobinsky', 'sbobinsky@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmez
 ('Will Robins', 'wrobins@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night,
-  parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, is_active)
+  parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
 
 VALUES (10, 'Speed Lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 
   'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 931, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', '28142', true),
@@ -14,11 +14,11 @@ VALUES (10, 'Speed Lamp', 'description', 'https://images.pexels.com/photos/20866
   'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 460, 0, 5, 6, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '44583', true);
 
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
-VALUES ('2018-09-11', '2018-09-26',8, 12),
-('2019-01-04', '2019-02-01', 8, 11),
-('2023-10-01', '2023-10-14', 9, 12);
+VALUES ('2018-09-11', '2018-09-26',2, 3),
+('2019-01-04', '2019-02-01', 2, 2),
+('2023-10-01', '2023-10-14', 1, 3);
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
-VALUES (12, 9, 13, 3, 'messages'),
-(11, 8, 14, 4, 'messages'),
-(12, 7, 15, 4, 'messages');
+VALUES (3, 2, 1, 3, 'messages'),
+(2, 2, 2, 4, 'messages'),
+(3, 1, 3, 4, 'messages');
